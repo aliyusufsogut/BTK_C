9900c40648ac7056baade5a2fa9074b822dfd65c
Ali Yusuf Sogut <aliyusufsogut@outlook.com>
2024-01-24 16:10:45 +0300

    6.4 projesi yapıldı

--- 6.4/main.c
+++ 6.4/main.c
@@ -4,5 +4,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	int i = 3;
+	printf("%d\n",i);
+		
+	i += 5; // i = i + 5
+	printf("%d\n",i);
+	
+	i -= 5; // i = i - 5
+	printf("%d\n",i);
+	
+	i *= 3; // i = i * 3
+	printf("%d\n",i);
+	
+	i /= 3; // i = i / 3
+	printf("%d\n",i);
+	
 	return 0;
 }
