8cdecb99341a26109bb91e99a81cc97c880121b1
Ali Yusuf Sogut <aliyusufsogut@outlook.com>
2024-01-24 16:37:54 +0300

    6.4.1 projesi yapıldı

--- 6.4/6.4.1/main.c
+++ 6.4/6.4.1/main.c
@@ -4,5 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int i;
+	
+	i = 0;
+	
+	printf("%d\n", i);
+	
+	printf("%d\n", i++ );  // önce mevuct deðer yazýlacak, sonra deðer artýrýlacak
+	
+	printf("%d\n", i);
+	
+	printf("%d\n", ++i);  // önce deðer artýrýlacak, sonra yeni deðer yazýlacak
+	
+	printf("%d\n", i);
+	
+	
 	return 0;
 }
