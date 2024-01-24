2bf73fe6db8221e36bd8301de841ca7cc41be4ec
Ali Yusuf Sogut <aliyusufsogut@outlook.com>
2024-01-25 02:31:57 +0300

    6.5.2. projesi yapıldı

--- 6.5/6.5.2/main.c
+++ 6.5/6.5.2/main.c
@@ -4,5 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	float cm, metre, km;
+	
+	printf("Uzunlugu santimetre cinsinden giriniz: ");
+	scanf("%f", &cm);
+	
+	metre = cm / 100.0;
+	km    = cm / 100000.0;
+	
+	printf("Metre cinsinden uzunluk = %.2f m \n", metre);
+	printf("Kilometre cinsinden uzunluk = %.2f km", km);
+	
 	return 0;
 }
