81b6db8346f6a76652b4d76399e432c2e2fb6a3b
Ali Yusuf Sogut <aliyusufsogut@outlook.com>
2024-01-25 02:16:42 +0300

    6.5.1 projesi yapıldı

--- 6.5/6.5.1/main.c
+++ 6.5/6.5.1/main.c
@@ -4,5 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	int a=35;    //10'luk sayý düzeninde (decimal)
+	int b=0b00100011;// 2'lik sayý düzenincde (binary)
+	int c=0x23;  // 16'lýk sayý düzeninde (hexadecimal)
+	
+	printf("a= %d\n", a);
+	printf("b= %d\n", b);
+	printf("c= %d\n", c);
+	
 	return 0;
 }
