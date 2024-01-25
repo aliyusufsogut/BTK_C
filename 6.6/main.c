02e0247c2c784766470c7dfdc2e9672f9c0f494d
Ali Yusuf Sogut <aliyusufsogut@outlook.com>
2024-01-25 17:36:57 +0300

    6.6 projesi yapıldı

--- 6.6/main.c
+++ 6.6/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	double taban, kuvvet, sonuc;
+	
+	
+	printf("Taban degerini giriniz: ");
+	scanf("%lf", &taban);
+	printf("Kuvvet degerini giriniz: ");
+	scanf("%lf", &kuvvet);
+	
+	sonuc = pow(taban, kuvvet);
+	
+	printf("%.2lf ^ %.2lf = %.2lf", taban, kuvvet, sonuc);
+	
 	return 0;
 }
