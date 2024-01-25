ebe6139fda2eb34f7de40d88ca67b8a4bce8a9ef
Ali Yusuf Sogut <aliyusufsogut@outlook.com>
2024-01-25 17:43:14 +0300

    6.6.1 projesi yapıldı

--- 6.6/6.6.1/main.c
+++ 6.6/6.6.1/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	double sayi, karekok;
+	
+	printf("Karekoku hesaplanacak sayiyi giriniz : ");
+	scanf("%lf", &sayi);
+	
+	karekok = sqrt(sayi);
+	
+	printf ("%.2lf sayisinin karekoku = %.2lf", sayi, karekok);
+	
 	
 	return 0;
 }
