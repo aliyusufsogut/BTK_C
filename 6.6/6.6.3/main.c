0e9bf3b4906b097e424bf8059b62a00b2721872d
Ali Yusuf Sogut <aliyusufsogut@outlook.com>
2024-01-25 18:01:43 +0300

    6.6.3 projesi yapıldı

--- 6.6/6.6.3/main.c
+++ 6.6/6.6.3/main.c
@@ -4,5 +4,25 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	float ana_para, zaman, faiz_orani, basit_faiz_miktari, bilesik_faiz_miktari;
+	
+	printf("Ana para miktarini giriniz: ");
+	scanf("%f", &ana_para);
+	
+	printf("Zamani giriniz: ");
+	scanf("%f", &zaman);
+	
+	printf("Faiz oranini giriniz: ");
+	scanf("%f", &faiz_orani);
+	
+	basit_faiz_miktari = (ana_para * zaman * faiz_orani) / 100;
+	
+	printf("Basit faiz hesabi ile hesaplanan faiz miktari = %f", basit_faiz_miktari);
+	
+	bilesik_faiz_miktari = ana_para * (pow((1+ faiz_orani / 100), zaman));
+	
+	printf("Bilesik faiz hesabi ile hesaplanan faiz miktari = %f", bilesik_faiz_miktari);
+	
 	return 0;
 }
